d4ca994c58b66919886fbc18daa928f48f0ffc14
Orad Reshef <oradwastaken@gmail.com>
2025-08-15 21:22:31 -0400

    Added function to get all players stats

--- src/fantasyhelper/stats.py
+++ src/fantasyhelper/stats.py
@@ -39,3 +39,64 @@ def get_roster(team_abbr, season="20242025") -> pd.DataFrame:
     df = df[["id", "firstName", "lastName", "positionCode", "team"]]
     df.set_index("id", inplace=True)
     return df
+
+
+def get_player_stats(season="20242025"):
+    player_stats_dfs = []
+    i = 1
+    while i > 0:
+        stats = client.stats.skater_stats_summary(
+            start_season=season,
+            end_season=season,
+            start=100 * (i - 1) + 1,
+            limit=100 * i,
+        )
+        if stats == []:
+            i = -1
+        else:
+            player_stats_dfs.append(pd.DataFrame(stats))
+            i += 1
+
+    df_all_stats = pd.concat(
+        player_stats_dfs,
+        ignore_index=True,
+    )
+
+    # If player has played for multiple teams, only keep the most recent one:
+    df_all_stats["teamAbbrevs"] = df_all_stats["teamAbbrevs"].str.split(",").str[-1]
+
+    df_all_stats = df_all_stats[
+        [
+            "playerId",
+            "skaterFullName",
+            "teamAbbrevs",
+            "positionCode",
+            "gamesPlayed",
+            "goals",
+            "assists",
+            "ppPoints",
+            "shPoints",
+            "gameWinningGoals",
+            "shots",
+        ]
+    ]
+
+    df_all_stats.rename(
+        columns={
+            "skaterFullName": "FullName",
+            "teamAbbrevs": "team",
+            "positionCode": "position",
+            "gamesPlayed": "GP",
+            "goals": "G",
+            "assists": "A",
+            "ppPoints": "PPP",
+            "shPoints": "SHP",
+            "shots": "SOG",
+            "gameWinningGoals": "GWP",
+        },
+        inplace=True,
+    )
+
+    df_all_stats.set_index("playerId", inplace=True)
+
+    return df_all_stats
